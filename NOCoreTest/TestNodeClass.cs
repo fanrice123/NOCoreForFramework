ff6e1f820aba65db9b082618ae2dbfda1d261f61
MxtfV <mustafa.logan@outlook.com.au>
2017-08-14 21:46:50 +1000

    Added a couple of tests for the edge class

---
+++ NOCoreTest/TestNodeClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NOCoreTest
+{
+    [TestClass]
+    public class TestNodeClass
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //TO DO:
+        }
+    }
+}
