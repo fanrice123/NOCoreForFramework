3c49a375bbadc9a9dada6b04c3af411876d2ea60
Chun Huei <fchunhuei@gmail.com>
2017-08-14 20:57:59 +1000

    Add new unit test case for K shortest path.

--- NOCoreTest/KShortestPathTest.cs
+++ NOCoreTest/KShortestPathTest.cs
@@ -32,10 +32,10 @@ namespace NOCoreTest
 			graph.ConnectNodeToWith(D, E, new Edge(7));
 
 			/*
-			graph.ConnectNodeToWith(B, C, new Edge(1));
-			graph.ConnectNodeToWith(C, D, new Edge(3));
-			graph.ConnectNodeToWith(D, C, new Edge(2));
-			graph.ConnectNodeToWith(E, C, new Edge(1));
+			graph.ConnectNodeToWith(B, C, new Edge()(1));
+			graph.ConnectNodeToWith(C, D, new Edge()(3));
+			graph.ConnectNodeToWith(D, C, new Edge()(2));
+			graph.ConnectNodeToWith(E, C, new Edge()(1));
 			*/
 
 			KShortestPath ksp = new KShortestPath(graph, A);
@@ -67,15 +67,89 @@ namespace NOCoreTest
 			graph.ConnectNodeToWith(D, E, new Edge(7));
 
 			/*
-			graph.ConnectNodeToWith(B, C, new Edge(1));
-			graph.ConnectNodeToWith(C, D, new Edge(3));
-			graph.ConnectNodeToWith(D, C, new Edge(2));
-			graph.ConnectNodeToWith(E, C, new Edge(1));
+			graph.ConnectNodeToWith(B, C, new Edge()(1));
+			graph.ConnectNodeToWith(C, D, new Edge()(3));
+			graph.ConnectNodeToWith(D, C, new Edge()(2));
+			graph.ConnectNodeToWith(E, C, new Edge()(1));
 			*/
 
 			KShortestPath ksp = new KShortestPath(graph, A);
 
 			Assert.AreEqual(1, ksp.PathsTo(E).Count);
+		}
+
+		[TestMethod]
+		public void TestFindKShortestPath3()
+		{
+			Graph graph = new Graph();
+
+			var A = new Node() { Label = "A" };
+			var B = new Node() { Label = "B" };
+			var C = new Node() { Label = "C" };
+			var D = new Node() { Label = "D" };
+			var E = new Node() { Label = "E" };
+			var F = new Node() { Label = "F" };
+			var G = new Node() { Label = "G" };
+			var H = new Node() { Label = "H" };
+			graph.AddNode(H);
+			graph.AddNode(A);
+			graph.AddNode(B);
+			graph.AddNode(C);
+			graph.AddNode(D);
+			graph.AddNode(E);
+			graph.AddNode(F);
+			graph.AddNode(G);
+
+			graph.ConnectNodeToWith(A, H, new Edge());
+			graph.ConnectNodeToWith(H, F, new Edge());
+			graph.ConnectNodeToWith(A, B, new Edge());
+			graph.ConnectNodeToWith(B, D, new Edge());
+			graph.ConnectNodeToWith(A, C, new Edge());
+			graph.ConnectNodeToWith(C, D, new Edge());
+			graph.ConnectNodeToWith(D, E, new Edge());
+			graph.ConnectNodeToWith(E, F, new Edge());
+			graph.ConnectNodeToWith(D, G, new Edge());
+			graph.ConnectNodeToWith(G, F, new Edge());
+
+			KShortestPath ksp = new KShortestPath(graph, A);
+
+			Assert.AreEqual(1, ksp.PathsTo(F).Count);
+		}
+
+		[TestMethod]
+		public void TestFindKShortestPath4()
+		{
+			Graph graph = new Graph();
+
+			var A = new Node() { Label = "A" };
+			var B = new Node() { Label = "B" };
+			var C = new Node() { Label = "C" };
+			var D = new Node() { Label = "D" };
+			var E = new Node() { Label = "E" };
+			var F = new Node() { Label = "F" };
+			var G = new Node() { Label = "G" };
+			graph.AddNode(A);
+			graph.AddNode(B);
+			graph.AddNode(C);
+			graph.AddNode(D);
+			graph.AddNode(E);
+			graph.AddNode(F);
+			graph.AddNode(G);
+
+			graph.ConnectNodeToWith(A, B, new Edge());
+			graph.ConnectNodeToWith(B, D, new Edge());
+			graph.ConnectNodeToWith(A, C, new Edge());
+			graph.ConnectNodeToWith(C, D, new Edge());
+			graph.ConnectNodeToWith(D, E, new Edge());
+			graph.ConnectNodeToWith(E, F, new Edge());
+			graph.ConnectNodeToWith(D, G, new Edge());
+			graph.ConnectNodeToWith(G, F, new Edge());
+
+			KShortestPath ksp = new KShortestPath(graph, A);
+
+			Assert.AreEqual(4, ksp.PathsTo(F).Count);
+			Assert.AreEqual(2, ksp.PathsTo(D).Count);
+			Assert.AreEqual(2, ksp.PathsTo(E).Count);
 		}
 	}
 }
