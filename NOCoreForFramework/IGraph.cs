738b9720e11c55f185a26719ae67317e60e3246d
Chun Huei <fchunhuei@gmail.com>
2017-09-07 04:16:42 +1000

    Remove docfx files. Documented IGraph.

--- NOCoreForFramework/IGraph.cs
+++ NOCoreForFramework/IGraph.cs
@@ -1,18 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace NetworkObservabilityCore
 {
 
+	/// <summary>
+	/// An arbitrary Graph type should always implement this interface.
+	/// </summary>
 	public interface IGraph : ICollection<INode>
 	{
+		/// <summary>
+		/// This property is a collection of <see cref="INode"/> of **IGraph**
+		/// </summary>
 		Dictionary<String, INode> AllNodes { get; }
 
 		Dictionary<String, IEdge> AllEdges { get; }
 
+		/// <summary>
+		/// This method connects 2 nodes with a directed edge.
+		/// </summary>
+		/// <param name="from">A node where a directed edge starts from.</param>
+		/// <param name="to">A node where a directed edge connects to.</param>
+		/// <param name="edge">A directed edges connects up <paramref name="from"/> and <paramref name="to"/></param>
 		void ConnectNodeToWith(INode from, INode to, IEdge edge);
 
+		/// <summary>
+		/// This methods removes an edge from **IGraph**.
+		/// </summary>
+		/// <param name="edge"></param>
+		/// <returns></returns>
 		bool Remove(IEdge edge);
 
     }
